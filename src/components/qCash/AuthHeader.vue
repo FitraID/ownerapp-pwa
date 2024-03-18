d68a746d769b2eeac918aaaa5293a7bca7dbb879
Novan Fitra <108254675+FitraID@users.noreply.github.com>
2024-03-18 19:57:29 +0700

    deploy build

--- src/components/qCash/AuthHeader.vue
+++ src/components/qCash/AuthHeader.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="mini-logo text-center my-4">
-    <router-link to="/intro"
-      ><img src="../../assets/cintabunda.png" height="40%" width="40%" alt=""
-    /></router-link>
+    <img src="../../assets/cintabunda.png" height="40%" width="40%" alt=""/>
     <h4 class="card-title mt-3">{{ title }}</h4>
   </div>
 </template>
